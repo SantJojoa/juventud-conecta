5148c447c61722f4652071537dd2595d5909ddfd
Santiago Jojoa <santiago.jojoan@gmail.com>
2025-03-24 22:22:18 -0500

    style: change  "create-event" form styles and add a confirm modal and empty modal

--- src/components/create_event/createEvent.jsx
+++ src/components/create_event/createEvent.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import Swal from 'sweetalert2';
+import { ToastContainer, toast } from 'react-toastify';
 import './CreateEvent.css';
 
 const CreateEvent = () => {
@@ -11,17 +13,71 @@ const CreateEvent = () => {
         location: '',
     });
     const [error, setError] = useState('');
-    const [success, setSuccess] = useState('');
+    const [invalidFields, setInvalidFields] = useState({});
 
     const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+
+        if (invalidFields[name]) {
+            setInvalidFields({ ...invalidFields, [name]: false });
+        }
+    };
+
+    const showSuccessModal = () => {
+        Swal.fire({
+            icon: 'success',
+            title: 'Evento Creado',
+            text: 'El evento se ha creado exitosamente',
+            confirmButtonText: 'Aceptar',
+            confirmButtonColor: '#198754',
+            customClass: {
+                popup: 'swal-custom-popup',
+                title: 'swal-custom-title',
+                content: 'swal-custom-confirm-button'
+            }
+        });
+    }
+
+    const validateForm = () => {
+
+        const newInvalidFields = {};
+        let isValid = true;
+
+        Object.keys(formData).forEach(field => {
+            if (!formData[field]) {
+                newInvalidFields[field] = true;
+                isValid = false;
+            } else {
+                newInvalidFields[field] = false;
+            }
+        });
+
+        setInvalidFields(newInvalidFields);
+
+        if (!isValid) {
+            toast.error('Por favor, completa todos los campos', {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                closeButton: false
+
+            });
+        }
+        return isValid;
     };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        if (!validateForm()) {
+            return;
+        }
         const token = localStorage.getItem("token"); // Aquí recuperas el token
         setError('');
-        setSuccess('');
 
         try {
             const token = localStorage.getItem("token");
@@ -43,7 +99,9 @@ const CreateEvent = () => {
 
             if (!response.ok) throw new Error(data.error || 'Error al crear el evento');
 
-            setSuccess('✅ Evento creado exitosamente');
+            showSuccessModal();
+
+
             setFormData({
                 title: '',
                 imageSrc: '',
@@ -60,18 +118,20 @@ const CreateEvent = () => {
     return (
         <div className="create-event-container">
             <h2>Crear Evento</h2>
-            <form onSubmit={handleSubmit}>
-                <input name="title" value={formData.title} onChange={handleChange} placeholder="Título del evento" required />
-                <input name="imageSrc" value={formData.imageSrc} onChange={handleChange} placeholder="URL de la imagen" required />
-                <textarea name="description" value={formData.description} onChange={handleChange} placeholder="Descripción" required />
-                <input name="schedule" value={formData.schedule} onChange={handleChange} placeholder="Horario (separado por comas)" required />
-                <input name="date" type="date" value={formData.date} onChange={handleChange} required />
-                <input name="location" value={formData.location} onChange={handleChange} placeholder="Ubicación" required />
+            <form onSubmit={handleSubmit} noValidate>
+                <input name="title" value={formData.title} onChange={handleChange} placeholder="Título del evento" required className={invalidFields.title ? 'invalid-field' : ''} />
+                <input name="imageSrc" value={formData.imageSrc} onChange={handleChange} placeholder="URL de la imagen" required className={invalidFields.imageSrc ? 'invalid-field' : ''} />
+                <textarea name="description" value={formData.description} onChange={handleChange} placeholder="Descripción" required className={invalidFields.description ? 'invalid-field' : ''} />
+                <input name="schedule" value={formData.schedule} onChange={handleChange} placeholder="Horario (separado por comas)" required className={invalidFields.schedule ? 'invalid-field' : ''} />
+                <input name="date" type="date" value={formData.date} onChange={handleChange} required className={invalidFields.date ? 'invalid-field' : ''} />
+                <input name="location" value={formData.location} onChange={handleChange} placeholder="Ubicación" required className={invalidFields.location ? 'invalid-field' : ''} />
                 <button type="submit">Crear Evento</button>
             </form>
 
             {error && <p className="error">{error}</p>}
-            {success && <p className="success">{success}</p>}
+
+            <ToastContainer
+            />
         </div>
     );
 };
