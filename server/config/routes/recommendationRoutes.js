55d7b47d83c0dcb52d8c539bca97193c541f06a2
Santiago Jojoa <santiago.jojoan@gmail.com>
2025-09-21 20:09:45 -0500

    Agregar lógica para recomendaciones personalizadas en el componente Carousel, incluyendo la visualización de eventos próximos y recomendados. Se implementa un nuevo estilo para las insignias de eventos en el carrusel.

--- server/config/routes/recommendationRoutes.js
+++ server/config/routes/recommendationRoutes.js
@@ -1,11 +1,9 @@
-// server/config/routes/recommendationRoutes.js
 const express = require('express');
 const { getRecommendations } = require('../controllers/recomendationController');
 const { authMiddleware } = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
-// Requiere estar logueado; usa el userId del token si prefieres:
 router.get('/users/:userId/recommendations', authMiddleware, getRecommendations);
 
 module.exports = router;
